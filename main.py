08ffa8dfe7f6cb620b34291207c9252bc4444d5d
appletime81 <itrimsl8696@gmail.com>
2022-05-17 20:34:41 +0800

    Update

--- main.py
+++ main.py
@@ -22,22 +22,29 @@ def get_column_names(file_name):
 def get_all_file_names(path):
     all_file_names = []
     for file_name in sorted(os.listdir(path)):
-        all_file_names.append(file_name)
+        if file_name.endswith(".xls"):
+            all_file_names.append(file_name)
     return all_file_names
 
 
-def get_all_column_names(file_name):
-    global ALL_COL_NAMES
-    data = pd.read_excel(file_name)
-    col_names = get_column_names(file_name)
+def get_all_column_names(file_names, col_names):
+    """
+    :param file_names: all_excel_file_names
+    :param col_names: get from MVP_Data_Summary.xlsx
+    :return: pd.DataFrame
+    """
+    new_col_names = list()
+    for file_name in file_names:
+        tempdata = pd.read_excel(file_name)
+        tempdata.dropna(inplace=True)
+        temp_col_names = (["Database"] + tempdata.iloc[:, 0].to_list())[29:]
+        for col_name in temp_col_names:
+            new_col_names.append(col_name)
+    new_col_names = col_names + list(set(new_col_names))
+    pprint(new_col_names)
 
 
 if __name__ == "__main__":
     column_name_file = "MVP_Data_Summary.xlsx"
-    col_names = get_column_names(column_name_file)
-    # pprint(col_names)
-
     all_excel_file_names = get_all_file_names(os.getcwd())
-    all_excel_file_names = [
-        file_name for file_name in all_excel_file_names if file_name != column_name_file
-    ]
+    get_all_column_names(all_excel_file_names, get_column_names(column_name_file))
--- test_regx.py
+++ test_regx.py
@@ -1,7 +1,22 @@
-import re
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# a = set(a)
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# b = set(b)
+# res = b.difference(a)
+# res.add(21)
+# print(res)
 
 
-a = "False Call Device (%)"
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+data = pd.read_excel('20_20220516102136_6612907_2346086.xls')
+col_names = ["Database"] + data.iloc[:, 0].to_list()
 
 
-print(re.sub(r" \(\W+\)", "", a))
+
+print(data)
+
+
